cc6afcb96cee24b1f0d3c54ce6c852d27a8b6ee1
Apple <apple@Apples-MacBook-Pro.local>
2023-04-02 08:41:44 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view />
+  <router-view></router-view>
+    <!-- <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+ 
+  </router-view> -->
 </template>
 
 <script>
@@ -9,3 +15,30 @@ export default defineComponent({
   name: 'App'
 })
 </script>
+
+<style scoped>
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
